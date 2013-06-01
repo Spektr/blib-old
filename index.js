af3efe552f260e32e8593b8e98eeafd87576951c
Spektr <spektr1987@rambler.ru>
2013-06-02 02:30:54 +0400

    final code chow

--- index.js
+++ index.js
@@ -22,22 +22,21 @@ window.blib =(function(){
             var block = $('<div />', {'class': target+" b-blib__block"});
 			var title = $('<div />', {'class': "b-blib__block-title", 'text': title});
 
-            title.on('click', function(){
-                $(this).parent().find('.b-blib__block-code').toggle(400);
-            });
+
 
             var content = $('<div />', {'class': "b-blib__block-content"});
-            var innerCode = $('<textarea />', {'class': "b-blib__block-code"});
-            content.load(file, function(){innerCode.text(content[0].innerHTML);});
+            var innerCode = $('<pre />', {'class': "b-blib__block-code"});
+            content.load(file, function(){
+                innerCode.text(content[0].innerHTML);
+                title.on('click', function(){
+                    $(this).parent().find('.b-blib__block-code').toggle(400);
+                });
+            });
             var note = $('<div />', {'class': "b-blib__block-note", 'text': note});
 
             innerCode.hide();
             block.append(title, content, note, innerCode);
             $('.b-blib').append(block);
-
-            var lol = document.getElementsByTagName('textarea');
-            console.log(content[0].outerHTML);
-            document.write("++"+content[0].outerHTML);
 		}
 		
 	};
