05a5b9baff75851e8128ef65089def17ed219dd5
Spektr <spektr1987@rambler.ru>
2013-06-26 15:30:41 +0400

    some correct b-menu for b-pic

--- p-/b-popup-menu/b-popup-menu.js
+++ p-/b-popup-menu/b-popup-menu.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
+	blib.include("b-/b-menu/b-menu");
+	
     $('.b-popup-menu__opener').on('click',function(){
         $('.b-popup-menu__content').toggleClass("b-popup-menu__content_closed",1500);
     });
