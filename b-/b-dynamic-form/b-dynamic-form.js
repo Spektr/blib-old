b0c4a96dafd84150d630f7dda620269ab20480dc
Spektr <spektr1987@rambler.ru>
2013-07-03 18:53:30 +0400

    new method for b-dynamic-form (text,password,checkbox)

--- b-/b-dynamic-form/b-dynamic-form.js
+++ b-/b-dynamic-form/b-dynamic-form.js
@@ -1,34 +1,107 @@
 $(function(){
-	
-	blib.build("form", createForm); //указываем процаку как обрабатывать такого рода запрос
 		
 	//функция создания формы из переданных данных
-	function createForm(data){
-		if(!data){return false;}
-		var result = [];
-		console.log(data);
-		//обрабатываем серверный ответ
-		var form = $('<form />', {'class':"b-dynamic-form", 'name': data['name'], 'action':data['action'],'method':data['method']});
+	var createForm = (function(){
 		
+		/*определяем базовый конструктор полей формы*/			//0_0 сломать этот подход при первой же возможности
+		var defaultAttr = (function(){
+			var attrArray = {'class':true, 'name':true};
+			return function(obj,attr){
+				for(i in attr){
+					if(i in attrArray){obj[i]=attr[i]};
+				}
+				return obj;
+			}
+		})();
+		//задаем обработчики значений
+		var funcs = {};	
 		
-		/*
-		for(var i=0; i<data['items'].length; i++){
-			var div = $('<div />', {'class':"b-dynamic-menu__cell", 'alt': data['items'][i]['title'], 'data-action':data['items'][i]['action']});
-			var span = $('<span />', {'class':"b-pic b-pic__amtel b-pic__amtel_"+data['items'][i]['action'].replace(/\_/g, '-')});
-			div.append(span);
-			result.push(div);
+		//текстовое поле
+		funcs['text']=function(obj){
+			var result = document.createElement("input");
+			result = defaultAttr(result, obj);
+			result.type="text";
+			if('value' in obj){result.defaultValue=obj.value;}
+			
+			if('label' in obj){
+				var label = document.createElement("label");
+				label.appendChild(result);
+				label.innerHTML+=obj.label;
+				result = label;
+			}
+			
+			return result;
 		}
-		*/
 		
-		//куда будет загружен
-		var place = data['place']||'.b-main-page__panel';
-		$(place).append(form);
-		//каким классом оформлен
-		if(data['class']){$(form).addClass(data['class']);};
+		//пароль
+		funcs['password']=function(obj){
+			var result = document.createElement("input");
+			result = defaultAttr(result, obj);
+			result.type="password";
+			if('value' in obj){result.defaultValue=obj.value;}
+			
+			if('label' in obj){
+				var label = document.createElement("label");
+				label.appendChild(result);
+				label.innerHTML+=obj.label;
+				result = label;
+			}
+			
+			return result;			
+		}
+		
+		//чекбокс
+		funcs['check']=function(obj){
+			var result = document.createElement("input");
+			result = defaultAttr(result, obj);
+			result.type="checkbox";
+			if(('value' in obj) && obj['value']=="checked"){result.checked=true;}
+			
+			if('label' in obj){
+				var label = document.createElement("label");
+				label.appendChild(result);
+				label.innerHTML+=obj.label;
+				result = label;
+				if(('value' in obj) && obj['value']=="checked"){result.children[0].checked=true;}
+			}
+			
+			return result;
+		}
+		
+		
+		
+		
+		//и обьявляем конструктор
+		var handle = function(obj){
+			return (typeof funcs[obj['type']] === 'function')?funcs[obj['type']](obj):false;
+		}
+		/*определяем базовый конструктор полей формы*/
+		
+		return function (data){
+			if(!data){return false;}
+			var result = [];
 
-		
-		
-	}//createForm
+			//обрабатываем серверный ответ
+			var form = $('<form />', {'class':"b-dynamic-form", 'name': data['name'], 'action':data['action'],'method':data['method']});
 
+			for(var arr = data['content'], len = arr.length, i=0; i<len; i++){
+				var elem = handle(arr[i]);
+				result.push(elem);
+			}
+			form.append(result);
+			
+			//куда будет загружен
+			var place = data['place']||'.b-main-page__panel';
+			$(place).append(form);
+			//каким классом оформлен
+			if(data['class']){$(form).addClass(data['class']);};
+		
+			
+			
+		}//createForm
+	})();
+	
+		
+	blib.build("form", createForm); //указываем процаку как обрабатывать такого рода запрос
 
 });
