993c7d445a06e1754eaf13135b639e8660731574
Spektr <spektr1987@rambler.ru>
2013-07-11 19:10:06 +0400

    refactor b-blib for support version of files and not send request to server if file is actual

--- b-/b-blib/b-blib.js
+++ b-/b-blib/b-blib.js
@@ -1,11 +1,12 @@
 window.blib =(function(){
 	var self = this;
+	var storageFlag = ('localStorage' in window && window['localStorage'] !== null)?true:false;
 	var head = document.getElementsByTagName('head')[0];
-	var cssCache = new Array();
-	var jsCache = new Array();
-    var css = new Array();
-	var js = new Array();
-
+	var css = {};
+	var js = {};
+	var version = (storageFlag)?localStorage.getItem('version'):1;		//(0_0 NaN .. 1)curent version, get from server (seconds to last modified .ini file)
+	var forceLoad = "";//?new="+version;	//postfix for force send request to server -> then it will answer 304(if file dont change) or 200 -> we set this file current version 
+	
 	/*jQuery simulate $() and $.ajax*/
 	var $ = function(){		
 		var els = document.getElementsByTagName('*');
@@ -69,72 +70,105 @@ window.blib =(function(){
 	}
 	$.ajax = ajax;
 	/*jQuery simulate $() and $.ajax*/
+	
+	/*blib functions*/
+	var getFiles = function(obj){
+		var arr=[];
+		for(key in obj){
+			if(obj[key]['list']){
+				arr = arr.concat(obj[key]['list']);
+			}else{
+				arr.push(key);
+			}
+		}
+		return arr;
+	}
+	/*blib functions*/
 
 	return {
 		//include css file
 		'css': function(cssFile, inCache){
-            cssFile = cssFile.toString();
-			
-            for(key in css){
-                if(cssFile==css[key]){return true;}
-            }
-			for(key in cssCache){
-                if(cssFile==cssCache[key]){return true;}
-            }
-			
+			cssFile = cssFile.toString();
+		
+			if((cssFile in css) && (css[cssFile]['version']==version)){
+				return this;
+			}else{
+				for(key in css){
+					var innerFiles = (css[key]['list'])?css[key]['list']:[];
+					for(var i=0, len=innerFiles.length;i<len;i++){
+						if(innerFiles[i]==cssFile){return this.css(key, innerFiles);}
+					}
+				}
+				
+				forceLoad="?new="+version;
+				delete css[cssFile];
+				var addededStyle = document.getElementById(cssFile);
+				if(addededStyle){						
+					addededStyle.parentNode.removeChild(addededStyle);
+				}
+				
+			}
+
 			var link  = document.createElement('link');
 			link.rel  = 'stylesheet';
 			link.type = 'text/css';
-			link.href = cssFile;
+			link.href = cssFile+forceLoad;
 			link.media = 'all';
+			link.id = cssFile;
 			if(head.appendChild(link)){
-				if(inCache){
-					cssCache.push(cssFile);
-					css=css.concat(inCache);
-				}else{
-					css.push(cssFile);
+				css[cssFile]={};
+				css[cssFile]['version']=version;
+				if(inCache && inCache.length){
+					css[cssFile]['list']=inCache;
 				}
-				
-				if('localStorage' in window && window['localStorage'] !== null){
-					localStorage.setItem("cssCache", JSON.stringify(cssCache));
+
+				if(storageFlag){
 					localStorage.setItem("css", JSON.stringify(css));
 				}
 			}
-			return true;
+			return this;
 		},
 		
 		//include js file
 		'js': function(jsFile, inCache){
 			jsFile = jsFile.toString();
 			
-			for(key in js){
-				if(jsFile==js[key]){
-					js.splice(key,1);
-					var addededScript = document.getElementById(jsFile);
-					if(addededScript){						
-						addededScript.parentNode.removeChild(addededScript);
+			console.log("-----start----");
+			console.log(jsFile);
+
+			
+			if(jsFile in js){
+				
+			//console.log(version);
+			console.log("-----finish----");
+				
+				forceLoad=(js[jsFile]['version']==version)?"":"?new="+version;
+				delete js[jsFile];
+				var addededScript = document.getElementById(jsFile);
+				if(addededScript){						
+					addededScript.parentNode.removeChild(addededScript);
+				}
+			}else{
+				for(key in js){
+					var innerFiles = (js[key]['list'])?js[key]['list']:[];
+					for(var i=0, len=innerFiles.length;i<len;i++){
+						if(innerFiles[i]==jsFile){return this.js(key, innerFiles);}
 					}
 				}
 			}
-			
-			for(key in jsCache){
-				if(jsFile==jsCache[key]){return this;}
-			}
-			
+
 			var script  = document.createElement('script');
 			script.id = jsFile;
-			script.src = jsFile;
+			script.src = jsFile+forceLoad;
 			script.type="text/javascript";
 			if(head.appendChild(script)){
-				if(inCache){
-					jsCache.push(jsFile);
-					js=js.concat(inCache);
-				}else{
-					js.push(jsFile);
+				js[jsFile]={};
+				js[jsFile]['version']=version;
+				if(inCache && inCache.length){
+					js[jsFile]['list']=inCache;
 				}
-							
-				if('localStorage' in window && window['localStorage'] !== null){
-					localStorage.setItem("jsCache", JSON.stringify(jsCache));
+
+				if(storageFlag){
 					localStorage.setItem("js", JSON.stringify(js));
 				}
 			}
@@ -159,9 +193,10 @@ window.blib =(function(){
 					}
 				});
 			}else{
-				/*document.addEventListener("DOMContentLoaded", function(){ */obj.js(file+'.js');/*}, false );*/
+				obj.js(file+'.js');
 			}
 		},
+		
 		
 		/*
 		//method for load all stylesheet/script in one file
@@ -172,34 +207,40 @@ window.blib =(function(){
 			var obj = this;
 			document.addEventListener("DOMContentLoaded", function(){
 				//забиваем имеющиеся кеши
-				if('localStorage' in window && window['localStorage'] !== null){
-					
-					if(localStorage.getItem('css')){css=JSON.parse(localStorage.getItem('css'));}
-					var arr = JSON.parse(localStorage.getItem('cssCache'));
+				if(storageFlag){
+
+					var arr = JSON.parse(localStorage.getItem('css'));
+					var allCss = (arr)?getFiles(arr):[];
 					for(key in arr){
-						obj.css(arr[key], []);
+						obj.css(key, arr[key]['list']||[]);
 					}
-	
-					if(localStorage.getItem('js')){js=JSON.parse(localStorage.getItem('js'));}
-					arr = JSON.parse(localStorage.getItem('jsCache'));
+
+					var arr = JSON.parse(localStorage.getItem('js'));
+					var allJs =  (arr)?getFiles(arr):[];
 					for(key in arr){
-						obj.js(arr[key], []);
+						obj.js(key, arr[key]['list']||[]);
 					}
 				}
 				
 
-				var requestData = "data="+JSON.stringify((dataObject['script']?{'css':css, 'js':js}:{'css':css, 'js':"orderOnly"}));
+				var requestData = "data="+JSON.stringify((dataObject['script']?{'css':allCss, 'js':allJs}:{'css':allCss, 'js':"orderOnly"}));
 				requestData += (dataObject['exception'])?"&exception="+JSON.stringify(dataObject['exception']):"&exception="+JSON.stringify([]);
 				requestData += (dataObject['order'])?"&order="+JSON.stringify(dataObject['order']):"&order="+JSON.stringify(["b-/b-jquery/b-jquery.js", "b-/b-jquery-ui/b-jquery-ui.js"]);
 				
+				console.log(requestData);
+				
 				$.ajax({
-					url:'/b-/b-blib/b-blib.php',
+					url:'b-/b-blib/b-blib.php',
 					data:requestData,
 					dataType: "json",
 					success: function(data){
+						console.log(data);
 						if(!data['status']){return false;}
-						if(data['css']){obj.css(data['css']['path'], data['css']['list']);}
-						if(data['js']){obj.js(data['js']['path'], data['js']['list']);}
+						version = data['version'];
+						if(storageFlag){localStorage.setItem("version", JSON.stringify(version));}
+						
+						if(data['css']){obj.css(data['css']['name'], data['css']['list']);}
+						if(data['js']){obj.js(data['js']['name'], data['js']['list']);}
 					}
 				});
 				
